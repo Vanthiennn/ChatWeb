b43f31302f00c527cdc3e269ec0e085693aa33ed
Thien <vanthien2k14@gmai.com>
2023-04-04 19:14:59 +0700

    merge code

--- client/src/stored/watcherStored.js
+++ client/src/stored/watcherStored.js
@@ -3,7 +3,7 @@ import * as ActionTypes from './actionTypes'
 import { call, put, select, takeEvery, takeLeading } from 'redux-saga/effects';
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
-const host = "https://chat-web-vanthiennn.vercel.app/"
+const host = "https://api-chat-web.vercel.app/"
 const fetchAPI = (method, url, data) => {
     return axios({
         method: method,
