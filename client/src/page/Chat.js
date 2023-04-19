9b4c9fddf17f80e8ea3e44c7b7b72c29a2e2ea0b
Thien <vanthien2k14@gmai.com>
2023-04-19 22:28:32 +0700

    remove console log

--- client/src/page/Chat.js
+++ client/src/page/Chat.js
@@ -54,7 +54,7 @@ export default function Chat({ isOnline }) {
         socketClients.on("connect", () => {
         })
         socketClients.on("room-receive", (data) => {
-          console.log(data,'dataaa')
+
           if (data) {
             setRoomID(data)
           }
@@ -97,7 +97,6 @@ export default function Chat({ isOnline }) {
       }
     }
   }, [user])
-  console.log(roomID,'roomID')
   return (
     <React.Fragment>
       <div className='chat'>
